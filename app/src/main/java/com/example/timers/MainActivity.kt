81b48ba59048b35f4bcdedaa94575752026036fe
Elisa <elisa-devops@users.noreply.github.com>
2024-10-22 09:30:32 -0500

    created duration logic for stopwatch secs/mins/hrs and output to user view

--- app/src/main/java/com/example/timers/MainActivity.kt
+++ app/src/main/java/com/example/timers/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.timers
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
 import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.time.AbstractLongTimeSource
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.measureTime
 
 class MainActivity : AppCompatActivity() {
+    var isRunning = false
+
+    object RealtimeMonotonicTimeSource : AbstractLongTimeSource(DurationUnit.NANOSECONDS) {
+        override fun read(): Long = SystemClock.elapsedRealtimeNanos()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,15 +39,32 @@ class MainActivity : AppCompatActivity() {
 
         val stopWatchButton = findViewById<TextView>(R.id.stopW_start)
         var elapsedHours = 0
+        var elapsedMins = 0
+        var elapsedSecs = 0
+        var elapsedMillis = 0
+
 
         if (stopWatchButton.text == "Start") {
             stopWatchButton.text = "Stop"
+            isRunning = true
             stopWatchButton.setBackgroundColor(getColor(R.color.red))
+            elapsedSecs = (System.currentTimeMillis().toInt()) / 1000
+            stopWatchSecs.text = elapsedSecs.toString()
+
+
+                var elapsed: Duration = RealtimeMonotonicTimeSource.measureTime {
+                    Thread.sleep(1000)
+                }
+                stopWatchSecs.text = elapsed.inWholeSeconds.toString()
+                stopWatchMins.text = (elapsed / 60).inWholeMinutes.toString()
+                stopWatchHours.text = (elapsed / 3600).inWholeHours.toString()
+
+
             /*while (stopWatchHours.text != "Start") {
                 elapsedHours = 15
             }*/
-            elapsedHours = 15
-            stopWatchHours.text = elapsedHours.toString()
+            //elapsedHours = 15
+            //stopWatchHours.text = elapsedHours.toString()
         } else {
             stopWatchButton.text = "Start"
             stopWatchButton.setBackgroundColor(getColor(R.color.brightTeal))
